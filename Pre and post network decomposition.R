7c0a9eb0ea858bfa521103f4f17f839b48f1030f
johnardavies <johnardavies@gmail.com>
2016-02-07 20:07:56 +0000

    Edited network code

--- Pre and post network decomposition.R
+++ Pre and post network decomposition.R
@@ -1,36 +1,47 @@
 
-#############################################################################
+##################################################################################################
 ##### The code below divides the following connections into the different kinds where there
 #is a before network labelled 'pre' and an after network labeled 'post'
+
+
+library("igraph") #The following use the R igraph package
+
+
+#############################################################################
+#1. Extracts the new unreciprocated connections
+#Labelled newunrec
 #############################################################################
 
-library('igraph') #The following use the R igraph package
+#The next command creates a new network that is the difference between the post and pre network and obtains the edges
+#b contains edges that are in the post network, but which are not in the pre network
+b<-graph.difference(post,pre, byname=FALSE)
 
-#############################################################################
-#1. Extracts the completely new unreciprocated connections
-#############################################################################
+###################################################################################################
+#The graph comparisons here are done on the vertex ids. This depends on the number of vertices remaining unchanging
+#if byname is set to true then the comparison is done by name; this requires the vertices to have name attributes
+###################################################################################################
 
-#deletes the connections in the post event network that are reciprocal
-postunrec<-delete.edges(post, which(is.mutual(post, es = E(post))=="TRUE"))
+#Calculates the post event network of reciprocal connections
+postrec<-delete.edges(post, which(is.mutual(post)=="FALSE"))
 
-#selects the unreciprocated connections that were not in the pre event network i.e. the new ones
-newunrec<-graph.difference(postunrec,pre)
+#selects the new connections that were not completely new reciprocal i.e. the new unreciprocated ones or 
+#the ones consolidating an existing connection
+newunrec1<-delete.edges(b, which(is.mutual(b)=="TRUE"))
+
+newunrec<-graph.difference(newunrec1, postrec, byname=FALSE)
 
 
 #############################################################################
 #2. Extracts the completely new reciprocated connections
+# Labelled newrec
 #############################################################################
 
-#The next two commands creates a new network that is the difference between the post and pre network and obtains the edges
-#b contains edges that are in the post network, but which are not in the pre network
-b<-graph.difference(post,pre)
-
 #Only select new connections that are completely reciprocal
-newrec<-delete.edges(b, which(is.mutual(b)=="FALSE)
+newrec<-delete.edges(b, which(is.mutual(b)=="FALSE"))
 
 #The below calculates the edges that were in the network before the event, but which have since disappeared
 #This is not used further, but is included for completeness
-edgesgone<-graph.difference(pre, post)
+edgesgone<-graph.difference(pre, post, byname=FALSE)
 
 
 ############################################################################
@@ -38,32 +49,31 @@ edgesgone<-graph.difference(pre, post)
 #  Labelled newold
 ############################################################################
 
-#Deletes the edges of the post event graph that are not reciprocal, to leave the post event reciprocal connections
-postrec<-delete.edges(post, which(is.mutual(post, es = E(post))=="FALSE"))
-
-#Extracts the reciprocal connections that are were formed by consolidation of an existing connection
-newreccon<-graph.difference(postrec, newrec)
+#Extracts the reciprocal connections that were formed by consolidation of an existing connection
+newreccon<-graph.difference(postrec, newrec, byname=FALSE)
 
 #Extracts the edges in the consolidated reciprocal connections that are new i.e. the new connections that created the reciprocal connection
-newoldint<-graph.difference(newreccon, pre)
+newold<-graph.difference(newreccon, pre, byname=FALSE)
 
 
 ###########################################################################
-#4. Extracts the connections that are in both the pre and post networks
-# Labelled preandpost
-
-preandpost<-graph.intersection(post, pre)
+#4. Extracts the connections that are in both the pre and post networks i.e. that stayed the same
+# Labelled preandpost 
+###########################################################################
+preandpost<-graph.intersection(post, pre, byname=FALSE)
 
 ###########################################################################
 #Checks that everything sums together
-#Connections in the post network =1. New unreciprocated connections (newunrec) + 2. New reciprocal connections (newrec) +3. New consolisated connections (newold) + 4. Connections from the pre network that are in the post network
+#Connections in the post network =1. New unreciprocated connections (newunrec) + 2. New reciprocal connections (newrec) +3. New consolisated connections (newold) + 4. Connections from the pre network that are in the post network (preandpost)
 
 #Sticks all the connections back together
-testgraph<-graph.union(newunrec, newrec, newold, preandpost)
+testgraph<-graph.union(newunrec, newrec, newold, preandpost, byname=FALSE)
 
-#Checks that it is the same graph
-graph.difference(post, testgraph) #should be 0
+ecount(post)-ecount(testgraph) #should be 0
 
+#Checks that it is the same graph should be no difference. The edges should be the same
+graph.difference(post, testgraph, byname=FALSE)
+##########################################################################
 
 
 
